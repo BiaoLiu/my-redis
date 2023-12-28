335dc94ee6d5889c4b9e6c8dbaff21d099b990f7
liubiao <liubiao2875@hotmail.com>
2023-12-28 18:46:26 +0800

    feat

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,16 @@
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-use bytes::Bytes;
-use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame, client, Result, Command};
-
-type Db = Arc<Mutex<HashMap<String, Bytes>>>;
-
-
+use tokio::fs::File;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    let mut file = File::open("foo.txt").await?;
+    let res = file.write(b"Some thing").await?;
+    println!("{:?}", res);
 
-    let db = Db::new(Mutex::new(HashMap::new()));
-    loop {
-        let (socket, _) = listener.accept().await.unwrap();
-
-        let db = db.clone();
-        tokio::spawn(async move {
-            process(socket, db).await;
-        });
-    }
-}
-
-async fn process(socket: TcpStream, db: Db) {
-    let mut connection = Connection::new(socket);
-    // let mut db = HashMap::new();
-
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
-
-        let response = match Command::from_frame(frame).unwrap() {
-            Command::Set(cmd) => {
-                let mut db = db.lock().unwrap();
-                db.insert(cmd.key().to_string(), cmd.value().clone());
-                Frame::Simple("OK".to_string())
-            }
-            Command::Get(cmd) => {
-                let db = db.lock().unwrap();
-                if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone().into())
-                } else {
-                    Frame::Null
-                }
-            }
-            cmd => panic!("unimplemented {:?}", cmd)
-        };
-        // Respond with an error
-        // let response = Frame::Error("unimplemented".to_string());
-        connection.write_frame(&response).await.unwrap();
-    }
+    let mut buffer = Vec::new();
+    let res2 = file.read_to_end(&mut buffer).await?;
+    println!("{:?}", res2);
+    Ok(())
 }
